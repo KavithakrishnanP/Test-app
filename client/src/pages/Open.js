b2d3d728db51d0eb810368e8440d2cf2577886fb
Kavitha Krishnan <kavithakrishnansk@gmail.com>
2023-07-07 09:48:38 +0530

    code added

---
+++ client/src/pages/Open.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+const Open = () => {
+  return (
+   
+      <div>
+       <h1 className='home'> Welcome to my Blog App 😊</h1>
+      </div>
+   
+  )
+}
+
+export default Open;
